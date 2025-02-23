9e7080288bc415d19627a2f32c3798fc5cef3c12
Lavanya-lahari <pullalavanya3@gmail.com>
2025-02-23 12:17:46 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 def check_even_odd(number):
-    if number % 2 == 0:
+    if number % 3 == 0:
         print(f"{number} is even.")
     else:
         print(f"{number} is odd.")
 
 # Test the function
 num = int(input("Enter a number: "))
-check_even_odd(num)
+check_even_odd(number)
